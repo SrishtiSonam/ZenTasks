2190e9d0739b75ae3fe090ea264037ffa3e7478c
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-09-25 18:01:56 +0530

    Database changes in django setting.

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
