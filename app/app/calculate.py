435e7c1fdf393cabe294e69cb73b31317c8ae356
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-09-25 14:04:25 +0530

    Installed psycopg2.

--- app/app/calculate.py
+++ app/app/calculate.py
@@ -2,13 +2,10 @@
     Calculator Function
 """
 
+def add(x, y):
+    """   Add x and y then return the result.   """
+    return x + y
 
-def add( x, y ):
-    """ Add x and y then return the result. """
-    return x+y
-
-
-def subtract( x, y ):
-    """ Subtract x and y then return the result. """
-    return x-y
-
+def subtract(x, y):
+    """   Subtract x and y then return the result.   """
+    return x - y
