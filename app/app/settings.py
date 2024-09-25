d1b55429c6c6de8554166220c77a22d18f1c910b
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-09-25 23:29:31 +0530

    Name change in setting.

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'devdb',
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         # 'NAME': os.environ.get('DB_NAME'),
