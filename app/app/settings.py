88745a69ce5326291ae6f4638542640af77a15e3
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-09-25 23:32:37 +0530

    Name change in setting.

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # 'NAME': 'devdb',
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
+        'NAME': str(os.environ.get('DB_NAME')),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
     }
