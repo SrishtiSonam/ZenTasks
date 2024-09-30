cdde321e765a5bdb67dd836bac6c4c829753e759
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-09-30 22:38:12 +0530

    Mocked cases for db race condition.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
-from django.db.utils import OperationalError        
+from django.db.utils import OperationalError
 # Error when database is not ready
 from django.core.management.base import BaseCommand
 
@@ -24,5 +24,5 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -6,17 +6,17 @@ from unittest.mock import patch
 # For mocking the behavior of database - simulate the db responses
 
 from psycopg2 import OperationalError as Psycopg2Error
-# One of the possible error that we can get 
+# One of the possible error that we can get
 #   when we are trying to connevt to db, before the db is ready.
 
 from django.core.management import call_command
-# Help of func provided by django, that allow us to simulate call the command 
-from django.db.utils import OperationalError        
-# Another operational error (exception) that may be thrown by the db, 
-# depending on what stage of the start up process it is. 
-from django.test import SimpleTestCase          
-# Base test case that we are goin to use.
-# Db not available, no need of migrations and all the things required for test database.
+# Help of func provided by django, that allow us to simulate call the command
+from django.db.utils import OperationalError
+# Another operational error (exception) that may be thrown by the db,
+# depending on what stage of the start up process it is.
+from django.test import SimpleTestCase
+# Db not available, 
+# no need of migrations and all the things required for test database.
 
 
 # patch for macking objects
@@ -25,7 +25,8 @@ from django.test import SimpleTestCase
 class CommandTest(SimpleTestCase):
     """ Test Commands """
 
-    def test_wait_for_db_ready(self, patched_check):            # patched_check due to decorator.
+    def test_wait_for_db_ready(self, patched_check):            
+        # patched_check due to decorator.
         """ Test waiting for db if db is ready - One Test Case """
         patched_check.return_value = True
 
@@ -38,9 +39,8 @@ class CommandTest(SimpleTestCase):
         """ Test waiting for the database when getting operational error. """
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        # Here we just mocked the delay suiation - 
+        # Here we just mocked the delay suiation -
         # Diff cases for 6 time is created.
-
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
