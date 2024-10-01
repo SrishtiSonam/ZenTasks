7984cba5303928534ab8c7d360d5ae572c354214
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-10-01 11:09:39 +0530

    Mocked cases for db race condition. Correction

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,6 +5,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
+from django.db import connections
 from django.db.utils import OperationalError
 # Error when database is not ready
 from django.core.management.base import BaseCommand
@@ -16,13 +17,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """ Entry point for command. """
         self.stdout.write('Waiting for database...')
-        db_up = False
-        while db_up is False:
+        db_conn = None
+        while not db_conn:
             try:
-                self.check(databases=['default'])
-                db_up = True
-            except (Psycopg2OpError, OperationalError):
+                db_conn = connections['default']
+            except (OperationalError, Psycopg2OpError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
+                
         self.stdout.write(self.style.SUCCESS('Database available!'))
